fe577a27267f988716b2fab205750caa1bee9a84
minimalparts <aurelie.herbelot@cantab.net>
2023-02-03 11:20:26 +0100

    Updated API code

--- django-backend/toppixapi/admin.py
+++ django-backend/toppixapi/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book, Country, Character, Continent, Country, Time
+from .models import Book, Country, Character, Continent, Country, Time, Topic, Setting
 
 # Register your models here.
 
@@ -8,3 +8,5 @@ admin.site.register(Character)
 admin.site.register(Continent)
 admin.site.register(Country)
 admin.site.register(Time)
+admin.site.register(Topic)
+admin.site.register(Setting)
